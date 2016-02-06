6b9c14286c7023f22a86ca2220d0f1da2f7fb4d5
Amit Sinha <amitsinha@Amits-MacBook-Pro.local>
2016-02-07 00:50:36 +0530

    initial commit

---
+++ Products/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Products
+//
+//  Created by Amit Sinha on 07/02/16.
+//  Copyright © 2016 Amit Sinha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Products/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Products
+//
+//  Created by Amit Sinha on 07/02/16.
+//  Copyright © 2016 Amit Sinha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
